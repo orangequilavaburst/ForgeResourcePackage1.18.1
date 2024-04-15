6cf3e56c9cab1b9976cb2fd34f9b261a14de5c2a
J8-Bit <orangequilavaburst@gmail.com>
2024-04-15 16:01:39 -0700

    laser drill attempt 2

--- src/main/java/com/idtech/item/custom/LaserDrillItem.java
+++ src/main/java/com/idtech/item/custom/LaserDrillItem.java
@@ -1,51 +1,54 @@
 package com.idtech.item.custom;
 
-import net.minecraft.client.renderer.entity.GuardianRenderer;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.ProjectileUtil;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BeaconBlock;
-import net.minecraft.world.level.block.FrostedIceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 
-public class LaserDrillItem extends PickaxeItem {
+public class LaserDrillItem extends Item {
 
     private final float miningRange;
+    private int lastSentState;
+    private BlockPos blockMiningPos;
 
-    public LaserDrillItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties, float miningRange) {
-        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    public LaserDrillItem(Properties pProperties, float miningRange) {
+        super(pProperties.stacksTo(1));
         this.miningRange = miningRange;
-    }
-
-    @Override
-    public boolean canAttackBlock(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer) {
-        return false;
+        this.lastSentState = 0;
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
 
         ItemStack itemStack = pPlayer.getItemInHand(pUsedHand);
-        BlockHitResult hit = pLevel.clip(new ClipContext(pPlayer.getEyePosition(), pPlayer.position().add(pPlayer.getLookAngle().scale(miningRange)), ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, pPlayer));
+        BlockHitResult hit = pLevel.clip(new ClipContext(pPlayer.getEyePosition(), pPlayer.getEyePosition().add(pPlayer.getLookAngle().scale(miningRange)), ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, pPlayer));
         if (hit.getType() != HitResult.Type.MISS){
 
             BlockPos blockPos = hit.getBlockPos();
             BlockState blockState = pLevel.getBlockState(blockPos);
-            //blockState.getDestroyProgress(pPlayer, pLevel, hit.getBlockPos());
-            blockState.attack(pLevel, blockPos, pPlayer);
 
             pLevel.addParticle(ParticleTypes.FLAME, hit.getLocation().x, hit.getLocation().y, hit.getLocation().z, 0.0f, 0.0f, 0.0f);
 
-            return InteractionResultHolder.success(itemStack);
+            this.blockMiningPos = blockPos;
+
+            pPlayer.startUsingItem(pUsedHand);
+            return InteractionResultHolder.consume(itemStack);
 
         }
 
@@ -53,12 +56,75 @@ public class LaserDrillItem extends PickaxeItem {
     }
 
     @Override
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity, int pTimeCharged) {
+        if (pLivingEntity instanceof Player pPlayer) {
+            pLevel.destroyBlockProgress(pLivingEntity.getId(), this.blockMiningPos, -1);
+            this.lastSentState = 0;
+        }
+        super.releaseUsing(pStack, pLevel, pLivingEntity, pTimeCharged);
+    }
+
+    @Override
+    public void onUseTick(Level pLevel, LivingEntity pLivingEntity, ItemStack pStack, int pRemainingUseDuration) {
+
+        if (pLivingEntity instanceof Player pPlayer) {
+            BlockHitResult hit = pLevel.clip(new ClipContext(pPlayer.getEyePosition(), pPlayer.getEyePosition().add(pPlayer.getLookAngle().scale(miningRange)), ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, pPlayer));
+            if (hit.getType() != HitResult.Type.MISS) {
+
+                BlockPos blockPos = hit.getBlockPos();
+                BlockState blockState = pLevel.getBlockState(blockPos);
+                //blockState.getDestroyProgress(pPlayer, pLevel, hit.getBlockPos());
+                if (pPlayer.getAbilities().instabuild) {
+                    pLevel.destroyBlock(blockPos, true, pLivingEntity);
+                }
+                else{
+                    if (this.blockMiningPos.equals(blockPos)) {
+                        int t = pStack.getUseDuration() - pRemainingUseDuration;
+                        this.incrementDestroyProgress(blockState, blockPos, t, pPlayer);
+                        float f1 = blockState.getDestroyProgress(pPlayer, pLevel, this.blockMiningPos);
+                        if (f1 >= 0.7F) {
+                            pLevel.destroyBlockProgress(pPlayer.getId(), this.blockMiningPos, -1);
+                            pLevel.destroyBlock(this.blockMiningPos, true, pPlayer);
+                            return;
+                        }
+                        pPlayer.displayClientMessage(Component.literal(String.format("Block progress: %d", this.lastSentState)), true);
+                    }
+                    else{
+                        pLevel.destroyBlockProgress(pPlayer.getId(), this.blockMiningPos, -1);
+                        this.lastSentState = 0;
+                        pPlayer.displayClientMessage(Component.literal("Missed block!"), true);
+                        this.use(pLevel, pPlayer, pPlayer.getUsedItemHand());
+                    }
+                }
+
+                pLevel.addParticle(ParticleTypes.FLAME, hit.getLocation().x, hit.getLocation().y, hit.getLocation().z, 0.0f, 0.0f, 0.0f);
+
+            }
+        }
+
+        super.onUseTick(pLevel, pLivingEntity, pStack, pRemainingUseDuration);
+    }
+
+    @Override
     public int getUseDuration(ItemStack pStack) {
-        return 12000;
+        return 120000;
     }
 
     @Override
     public UseAnim getUseAnimation(ItemStack pStack) {
         return UseAnim.CROSSBOW;
     }
+
+    private float incrementDestroyProgress(BlockState pState, BlockPos pPos, int pUseDuration, Player pPlayer) {
+        int i = pUseDuration;
+        float f = pState.getDestroyProgress(pPlayer, pPlayer.level(), pPos) * (float)(i + 1);
+        int j = (int)(f * 10.0F);
+        if (j != this.lastSentState) {
+            pPlayer.level().destroyBlockProgress(pPlayer.getId(), pPos, j);
+            this.lastSentState = j;
+        }
+
+        return f;
+    }
+
 }
